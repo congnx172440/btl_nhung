e70851935ffbf5e5fa970af3821ac7a93d260736
congnx172440 <71223734+congnx172440@users.noreply.github.com>
2021-12-18 13:26:20 +0700

    add Vercel configs

--- config/database.php
+++ config/database.php
@@ -43,13 +43,14 @@ return [
             'foreign_key_constraints' => env('DB_FOREIGN_KEYS', true),
         ],
 
+        
         'mysql' => [
             'driver' => 'mysql',
             'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => env('DB_HOST', 'sql6.freemysqlhosting.net'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
+            'database' => env('DB_DATABASE', 'sql6459680'),
+            'username' => env('DB_USERNAME', 'sql6459680'),
             'password' => env('DB_PASSWORD', ''),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
