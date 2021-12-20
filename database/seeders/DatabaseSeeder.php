890ae369e2d881c4b456817edf9ff3280f8f65d4
congnx172440 <71223734+congnx172440@users.noreply.github.com>
2021-12-21 01:11:51 +0700

    add code

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,5 +15,24 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+        DB::table('vi_tris')->insert([
+           ['vi_tri'=>'Admin'],
+           ['vi_tri'=>'Quản lý'],
+           ['vi_tri'=>'Lãnh đạo'],
+           ['vi_tri'=>'Nhân viên']
+        ]);
+        DB::table('cong_ties')->insert([
+                ['ten_cong_ty'=>'Set',
+                    'dia_chi_cong_ty'=>'C9_BKHN',
+                'id_rfid'=>0,]
+        ]);
+        DB::table('users')->insert([
+            ['name'=>'Admin',
+            'email'=>'admin',
+            'password'=>bcrypt('12345'),
+            'id_vi_tri'=>1,
+            'id_cong_ty'=>1,
+            'id_rfid'=>0,]
+        ]);
     }
 }
